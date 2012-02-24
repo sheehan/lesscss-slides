5c2ffc260ac58cf5db68ff3ee1c4e8fbf25073c3
Matt Sheehan <matt.sheehan@objectpartners.com>
2012-02-24 16:26:33 -0600

    added css editor. other updates

--- js/main.js
+++ js/main.js
@@ -1,6 +1,55 @@
-$(function() {
-	// Deck initialization
-	$.deck('.slide');
-	
-	less.watch();
+$(function () {
+    $.deck('.slide');
+
+    less.watch();
+
+    var createEditorPair = function (el) {
+        ({
+            init:function () {
+                this.parser = new (less.Parser);
+
+                this.lessEditor = CodeMirror.fromTextArea(el, {
+                    mode:'css',
+                    onUpdate:this.syncEditors.bind(this)
+                });
+                $(this.lessEditor.getWrapperElement()).addClass('less');
+
+                var css = this.parseCss(this.lessEditor.getValue());
+
+                this.cssEditor = CodeMirror(function (e) {
+                    $(this.lessEditor.getWrapperElement()).after(e);
+                }.bind(this), {
+                    value:css,
+                    mode:'css'
+                });
+
+                $(this.cssEditor.getWrapperElement()).addClass('css');
+
+            },
+
+            syncEditors:function () {
+                if (this.lessEditor) {
+                    var css = this.parseCss(this.lessEditor.getValue());
+                    if (this.cssEditor && css !== null) {
+                        this.cssEditor.setValue(css);
+                    }
+                }
+            },
+
+            parseCss:function () {
+                var css = null;
+                this.parser.parse(this.lessEditor.getValue(), function (err, tree) {
+                    if (!err) {
+                        css = tree.toCSS();
+                    }
+                });
+                return css;
+            }
+        }.init());
+    };
+
+    $('textarea.less').each(function (index, el) {
+        createEditorPair(el);
+    });
+
 });
