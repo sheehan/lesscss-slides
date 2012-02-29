ed22ecff70852d3275f40d96bf79fddd13155d62
Matt Sheehan <matt.sheehan@objectpartners.com>
2012-02-28 22:30:04 -0600

    styles and misc

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-slides for less css webinar
+slides for less css webinar 5/13/2012
--- js/main.js
+++ js/main.js
@@ -8,23 +8,21 @@ $(function () {
             init:function () {
                 this.parser = new (less.Parser);
 
-                this.lessEditor = CodeMirror.fromTextArea(el, {
+                var lessCode = $(el).text();
+                var css = this.parseCss(lessCode);
+
+                this.$el = $('<div class="editor-pair"><div class="less-wrapper"><div class="less-header">LESS</div></div><div class="css-wrapper"><div class="css-header">CSS</div></div></div>').replaceAll(el);
+
+                this.lessEditor = CodeMirror(this.$el.find('.less-wrapper')[0], {
+                    value: lessCode,
                     mode:'css',
                     onUpdate:this.syncEditors.bind(this)
                 });
-                $(this.lessEditor.getWrapperElement()).addClass('less');
 
-                var css = this.parseCss(this.lessEditor.getValue());
-
-                this.cssEditor = CodeMirror(function (e) {
-                    $(this.lessEditor.getWrapperElement()).after(e);
-                }.bind(this), {
+                this.cssEditor = CodeMirror(this.$el.find('.css-wrapper')[0], {
                     value:css,
                     mode:'css'
                 });
-
-                $(this.cssEditor.getWrapperElement()).addClass('css');
-
             },
 
             syncEditors:function () {
@@ -36,9 +34,9 @@ $(function () {
                 }
             },
 
-            parseCss:function () {
+            parseCss:function (lessCode) {
                 var css = null;
-                this.parser.parse(this.lessEditor.getValue(), function (err, tree) {
+                this.parser.parse(lessCode, function (err, tree) {
                     if (!err) {
                         css = tree.toCSS();
                     }
