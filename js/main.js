a3df671356dbf2a5f254a62a2ed0dad27ffae0b2
Matt Sheehan <matt.sheehan@objectpartners.com>
2012-03-10 19:38:24 -0600

    moved editors to one panel. fix editor key propagation, other misc

--- js/main.js
+++ js/main.js
@@ -1,29 +1,57 @@
 $(function () {
+    var mainLayout = $('body').layout({
+        center__paneSelector:'.deck-container-wrapper',
+        east__paneSelector:'.editors-wrapper',
+        east__size:400,
+        onresize:function () {
+            $(document).trigger('mainLayout.resize');
+        }
+    });
+
     $.deck('.slide');
 
-    less.watch();
-
     var createEditorPair = function (el) {
-        ({
+        return ({
             init:function () {
                 this.parser = new (less.Parser);
 
-                var lessCode = $(el).text();
-                var css = this.parseCss(lessCode);
+//                var lessCode = 'div {}';//$(el).text();
+//                var css = this.parseCss(lessCode);
 
-                this.$el = $('<div class="editor-pair"><div class="less-wrapper"><div class="less-header">LESS</div></div><div class="css-wrapper"><div class="css-header">CSS</div></div></div>').replaceAll(el);
+//                this.$el = $('<div class="editor-pair"><div class="less-wrapper"><div class="less-header">LESS</div></div><div class="css-wrapper"><div class="css-header">CSS</div></div></div>').replaceAll(el);
+                this.$el = $(el);
+                this.$el.html('<div class="editors"><div class="less"></div><div class="css"></div></div>');
 
-                this.lessEditor = CodeMirror(this.$el.find('.less-wrapper')[0], {
-                    value: lessCode,
-                    mode:'css',
-                    onUpdate:this.syncEditors.bind(this)
+                var innerLayout = this.$el.find('.editors').height('100%').layout({
+                    center__paneSelector:'.less',
+                    south__paneSelector:'.css',
+                    south__size:'50%'
+                });
+                $(document).bind('mainLayout.resize', function() {
+                    innerLayout.resizeAll();
                 });
 
-                this.cssEditor = CodeMirror(this.$el.find('.css-wrapper')[0], {
-                    value:css,
+                this.lessEditor = CodeMirror(this.$el.find('.less')[0], {
+//                    value:lessCode,
                     mode:'css',
-                    readOnly: true
+                    onUpdate:this.syncEditors.bind(this),
+                    onKeyEvent: function(editor, event) {
+                        $.Event(event).stopPropagation();
+                    }
                 });
+
+                this.cssEditor = CodeMirror(this.$el.find('.css')[0], {
+//                    value:css,
+                    mode:'css',
+                    readOnly:true
+                });
+
+                return this;
+            },
+
+            setLess:function (less) {
+                this.lessEditor.setValue(less);
+                this.syncEditors();
             },
 
             syncEditors:function () {
@@ -47,8 +75,27 @@ $(function () {
         }.init());
     };
 
-    $('.less').each(function (index, el) {
-        createEditorPair(el);
+    var editorPair = createEditorPair($('.editors-wrapper'));
+
+
+    var processSlide = function($slide) {
+        var less = $slide.children('.less').hide().text();
+        if (less) {
+            mainLayout.open('east');
+            editorPair.setLess(less);
+        } else {
+            mainLayout.close('east');
+        }
+    };
+
+    $(document).bind('deck.change', function (event, from, to) {
+        processSlide($.deck('getSlide', to));
     });
+
+    processSlide($.deck('getSlide'));
+
+//    $('.less').each(function (index, el) {
+//        createEditorPair(el);
+//    });
 
 });
